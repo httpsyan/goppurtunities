257bedb7b62cd4ceebfb203ef3a8920c471442e7
Yan Castro <yancastrohenrique1@gmail.com>
2023-11-04 23:36:15 -0300

    feat: add request

--- handler/request.go
+++ handler/request.go
@@ -40,3 +40,20 @@ func (r *CreateOpeningRequest) Validate() error{
  }
  return nil
 }
+
+
+type updateOpeningRequest struct {
+	Role string `json:"role"`
+	Company string `json:"company"`
+	Location string `json:"location"`
+	Remote *bool `json:"remote"`
+	Link string `json:"link"`
+	Salary int64 `json:"salary"`
+}
+
+func (r *updateOpeningRequest) Validate() error {
+	if r.Role != "" || r.Company != "" || r.Location != "" || r.Remote != nil || r.Link != "" || r.Salary > 0 {
+		return nil
+	}
+	return fmt.Errorf("at least one field on valid field must be provided")
+}
